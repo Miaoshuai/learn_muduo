80debbd6c3824b73682a723079ac74616a01a8e0
miaoshua <945970809@qq.com>
2015-10-28 17:45:57 +0800

    一个空基类

---
+++ muduo/base/copyable.h
@@ -0,0 +1,18 @@
+/*======================================================
+    > File Name: copyable.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年10月28日 星期三 17时42分03秒
+ =======================================================*/
+
+#ifndef MUDUO_BASE_COPYABLE_H
+#define MUDUO_BASE_COPYABLE_H
+
+namespace muduo
+{
+    class copyable
+    {};
+};
+
+#endif

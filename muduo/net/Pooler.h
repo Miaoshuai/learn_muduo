437414609d748a8cf5330d27ed5a17163172d009
miaoshua <945970809@qq.com>
2015-11-04 23:38:17 +0800

    I/O复用基类

---
+++ muduo/net/Pooler.h
@@ -0,0 +1,61 @@
+/*======================================================
+    > File Name: Pooler.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年11月04日 星期三 14时48分28秒
+ =======================================================*/
+
+#ifndef MUDUO_NET_POLLER_H
+#define MUDUO_NET_POLLER_H
+
+#include <map>
+#include <vector>
+#include <boost/noncopyable.hpp>
+
+#include <muduo/base/Timestamp.h>
+#include <muduo/net/EventLoop.h>
+
+namespace muduo
+{
+namespace net
+{
+    class Channel;
+
+    class Poller : boost::noncopyable
+    {
+        public:
+            typedef std::vector<Channel *> ChannelList;
+            
+            Poller(EventLoop *loop);
+
+            virtual ~Poller();
+
+            //不许在I/O线程中调用,I/O复用的封装
+            virtual Timestamp poll(int timeoutMs,ChannelList *activeChannels) = 0;
+
+            //跟新Channel
+            virtual void updateChannel(Channel *channel) = 0;
+
+            //移除Channel
+            virtual void removeChannel(Channel *channel) = 0;
+
+            virtual bool hasChannel(Channel *channel)const;
+
+            static Poller *newDefaultPoller(EventLoop *loop);
+
+            void assertInLoopThread()const
+            {
+                ownerLoop_->assertInLoopThread();
+            }
+
+        protected:
+            typedef std::map<int,Channel*> ChannelMap;
+            ChannelMap Channels_;
+        private:
+            EventLoop *owerLoop_;
+    };
+}
+}
+
+#endif

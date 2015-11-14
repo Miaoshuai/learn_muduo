0514f87d7990b6ff07ebe8331b9bb22238b7b0a5
miaoshua <945970809@qq.com>
2015-11-14 23:16:58 +0800

    EpollPoller类的定义

--- muduo/net/EpollPoller.h
+++ muduo/net/EpollPoller.h
@@ -15,23 +15,23 @@ class EPollPoller : public Poller
 {
     public:
         EPollPoller(EventLoop *loop);
-        virtual Timestamp poll(int timeoutMs,ChannelList *activeChannels);
+        virtual Timestamp poll(int timeoutMs,ChannelList *activeChannels);   //内部调用epoll_wait函数
 
         virtual void updateChannel(Channel *channel);
 
-        virtual void removeChannel(Channel *Channel);
+        virtual void removeChannel(Channel *Channel);       
     private:
-        static const int kInitEventListSize = 16;
+        static const int kInitEventListSize = 16;           //epoll事件表的大小
 
         static const char *operatoionToString(int op);
 
-        void fillActiveChannels(int numEvents,ChannelList *activeChannels)const;
+        void fillActiveChannels(int numEvents,ChannelList *activeChannels)const; //将epoll返回的活跃事件填充到activeChannel
 
-        void update(int operation,Channel *channel);
+        void update(int operation,Channel *channel);    //对Channel的更改操作
 
         typedef std::vector<struct epoll_event> EventList;
 
-        int epollfd_;       //epoll的fd
+        int epollfd_;       //epoll的事件表fd
 
-        EventList events_;
+        EventList events_;  //epoll事件数组
 };

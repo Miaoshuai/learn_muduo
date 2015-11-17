bbe6b8dd0a78022fe44e78c77d6f13d4699e272f
miaoshua <945970809@qq.com>
2015-11-17 21:51:48 +0800

    EventLoopThread类的实现

---
+++ muduo/net/EventLoopThread.cc
@@ -0,0 +1,103 @@
+/*======================================================
+    > File Name: EventLoopThread.cc
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2015年11月17日 星期二 18时34分45秒
+ =======================================================*/
+
+#include <muduo/net/EventLoopThread.h>
+
+#include <muduo/net/EventLoop.h>
+
+#include <boost/bind.hpp>
+
+using namespace muduo;
+using namespace muduo::net;
+
+EventLoopThread::EventLoopThread(const ThreadInitCallback &cb,const string &name)
+    :loop_(NULL),
+    exiting_(false),
+    thread_(boost::bind(&EventLoopThread::threadFun,this),name),
+    mutex_(),
+    cond_(mutex_),
+    callback_(cb)
+{
+
+}
+
+EventLoopThread::~EventLoopThread()
+{
+    exiting_ = true;
+    if(loop_ != NULL)
+    {
+        loop_->quit();
+        thread_.join();
+    }
+}
+
+
+EventLoop *EventLoopThread::startLoop()
+{
+    assert(!thread_.started());
+    thread_.start();
+
+    {
+        MutexLockGuard lock(mutex_);
+        while(loop_ == NULL)
+        {
+            cond_.wait();
+        }
+    }
+}
+
+
+//线程函数
+void EventLoopThread::threadFunc()
+{
+    EventLoop loop;
+
+    if(callback_)
+    {
+        callback_(&loop);
+    }
+
+    {
+        MutexLockGuard lock(mutex_);
+        loop_ = &loop;
+        cond_.notify();
+    }
+
+    loop.loop();
+    loop_ = NULL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
